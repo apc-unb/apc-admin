a90218d96281785a25b27cc88cbb5fa3e403057c
Vitor F Dullens <vitordullens2.0@gmail.com>
2019-09-30 02:19:13 -0300

    starting classes crud

--- src/pages/schoolClass/index.js
+++ src/pages/schoolClass/index.js
@@ -1,13 +1,102 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardActions,
+  Typography,
+} from '@material-ui/core'
 
-function schoolClass() {
-    return (
-      <div className="schoolClass-page">
-        <h1>Hello School Class</h1>
-      </div>
-    );
+function SchoolClass() {
+  
+  const [classes, setClasses] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+      const response = await api.get('/classes')
+
+      setClasses(response.data)
+    }
+    getData()
+
+  }, [classes])
+  
+  async function handleAdd() {
+    // const data = [
+    //   {
+    //     professorfirstname,
+    //     professorlastname,     
+    //     classname,           
+    //     address,             
+    //     year,                 
+    //     season,                
+    //   }
+    // ]
+    // try{
+    //   const res = await api.post('/classes', data)
+    //   console.log(res.status)
+    // } catch(err){
+    //   console.error(err)
+    // }
+  }
+
+  function handleEdit(id) {
+
+  }
+
+  async function handleDelete(id) {
+    if(window.confirm("Deseja mesmo deletar esta?")) {
+      try {
+        const res = await api.delete(`/classes`, { data: [ {"id": id } ] } )
+        console.log(res.status)
+      } catch(err) {
+        console.error(err)
+      }
+    }
+  }
+
+
+  return (
+    <div className="schoolClass-page">
+      <h1>Turmas de APC</h1>
+      <br/>
+      <Button variant="contained" raised="true" color="primary" onClick={()=>handleAdd()}>
+        Adicionar
+      </Button>
+      <br/><br/>
+      {classes.map( c => (
+      <div key={c.ID}>
+        <Card className={classes.card}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              { c.classname } - { c.year }/{ c.season }
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Professor(a): {c.professorfirstname} {c.professorlastname}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Local: { c.address }
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary" onClick={() => handleEdit(c.ID)}>
+            Editar
+          </Button>
+          <Button size="small" color="primary" onClick={() => handleDelete(c.ID)}>
+            Deletar
+          </Button>
+        </CardActions>
+      </Card>
+      <br/>
+    </div>
+    ))}
+    </div>
+  );
 }
 
-export default schoolClass
+export default SchoolClass
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
 
-import schoolClass from './pages/schoolClass'
+import SchoolClass from './pages/schoolClass'
 import Student from './pages/student'
 import Home from './pages/home'
 import Exam from './pages/exam'
@@ -16,9 +16,9 @@ function Routes() {
             <Switch>
                 <Route exact path = "/" component= { Home } />
                 <Route exact path = "/news" component= { News } />
-                <Route exact path = "/exam" component= { Exam } />
-                <Route exact path = "/student" component= { Student } />
-                <Route exact path = "/class" component= { schoolClass } />
+                <Route exact path = "/exams" component= { Exam } />
+                <Route exact path = "/students" component= { Student } />
+                <Route exact path = "/classes" component= { SchoolClass } />
             </Switch>
         </BrowserRouter>
     )
