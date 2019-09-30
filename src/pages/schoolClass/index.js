67be96bb65edbfac4886236d2a6ed61b1db49c6f
Vitor F Dullens <vitordullens2.0@gmail.com>
2019-09-30 03:42:02 -0300

    changes in design

--- src/pages/schoolClass/index.js
+++ src/pages/schoolClass/index.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import api from '../../services/api'
+import Popup from "reactjs-popup"
+
 import {
   Button,
   Card,
   CardActionArea,
   CardContent,
   CardActions,
+  TextField,
   Typography,
 } from '@material-ui/core'
+
+import CloseIcon from '@material-ui/icons/Close'
 
 function SchoolClass() {
   
@@ -24,26 +29,11 @@ function SchoolClass() {
   }, [classes])
   
   async function handleAdd() {
-    // const data = [
-    //   {
-    //     professorfirstname,
-    //     professorlastname,     
-    //     classname,           
-    //     address,             
-    //     year,                 
-    //     season,                
-    //   }
-    // ]
-    // try{
-    //   const res = await api.post('/classes', data)
-    //   console.log(res.status)
-    // } catch(err){
-    //   console.error(err)
-    // }
+
   }
 
-  function handleEdit(id) {
-
+  async function handleEdit(event) {
+    
   }
 
   async function handleDelete(id) {
@@ -62,9 +52,15 @@ function SchoolClass() {
     <div className="schoolClass-page">
       <h1>Turmas de APC</h1>
       <br/>
-      <Button variant="contained" raised="true" color="primary" onClick={()=>handleAdd()}>
-        Adicionar
-      </Button>
+      {/* ADD BUTTON POPUP */}
+      <Popup trigger={
+            <Button variant="contained" raised="true" color="primary" onClick={()=>handleAdd()}>
+            Adicionar
+            </Button>
+          } position="right center">
+            <div>Popup content here !!</div>
+          </Popup>
+      {/* END OF ADD BUTTON POPUP */}
       <br/><br/>
       {classes.map( c => (
       <div key={c.ID}>
@@ -83,9 +79,74 @@ function SchoolClass() {
           </CardContent>
         </CardActionArea>
         <CardActions>
-          <Button size="small" color="primary" onClick={() => handleEdit(c.ID)}>
-            Editar
-          </Button>
+          {/* EDIT BUTTON POPUP */}
+          <Popup trigger={
+            <Button size="small" color="primary">
+              Editar
+            </Button>
+          } position="right center" modal>
+            {close => (
+              <div className="modal">
+              <Button size="small" color="primary" onClick={close}><CloseIcon /></Button>
+              <form onSubmit={() => handleEdit()}>
+              <Card className={classes.card}>
+              <CardActionArea>
+                <CardContent>
+                <TextField
+                  id="standard-name"
+                  name="classname"
+                  label="Turma"
+                  className={classes.textField}
+                  value={c.classname}
+                  margin="normal"
+                />
+                <TextField
+                  id="year"
+                  label="Ano"
+                  className={classes.textField}
+                  value={c.year}
+                  margin="normal"
+                />
+                <TextField
+                  id="season"
+                  label="Período"
+                  className={classes.textField}
+                  value={c.season}
+                  margin="normal"
+                />
+                <TextField
+                  id="professorfirstname"
+                  label="Nome do professor"
+                  className={classes.textField}
+                  value={c.professorfirstname}
+                  margin="normal"
+                />
+                <TextField
+                  id="professorlastname"
+                  label="Sobrenome do professor"
+                  className={classes.textField}
+                  value={c.professorlastname}
+                  margin="normal"
+                />
+                <TextField
+                  id="address"
+                  label="Local"
+                  className={classes.textField}
+                  value={c.address}
+                  margin="normal"
+                />
+                </CardContent>
+              </CardActionArea>
+              </Card>
+              <br/>
+              <Button variant="contained" size="small" color="primary" onClick={close}>
+                Editar
+              </Button>
+              </form>
+              </div>
+            )}
+          </Popup>
+          {/* END OF EDIT BUTTON POPUP */}
           <Button size="small" color="primary" onClick={() => handleDelete(c.ID)}>
             Deletar
           </Button>
